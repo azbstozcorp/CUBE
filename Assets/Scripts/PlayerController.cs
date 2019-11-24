2d4ff7032dbe033a93e0ce7fc8cbff3c8a72497d
Gavin White <horace5413@gmail.com>
2019-11-24 12:09:04 -0800

    Basic Movement
    cube

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody body;
+
+    [SerializeField]
+    Vector3 forceForward;
+    [SerializeField]
+    Vector3 forceSideways;
+
+    [SerializeField]
+    KeyCode keyForward;
+    [SerializeField]
+    KeyCode keyBackward;
+    [SerializeField]
+    KeyCode keyRight;
+    [SerializeField]
+    KeyCode keyLeft;
+
+    void Start() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+
+        if (Input.GetKey(keyForward)) body.velocity += forceForward;
+        if (Input.GetKey(keyBackward)) body.velocity -= forceForward;
+        if (Input.GetKey(keyRight)) body.velocity += forceSideways;
+        if (Input.GetKey(keyLeft)) body.velocity -= forceSideways;
+    }
+}
